723fd654d192ed2ac2216ad28306a64617964967
Ekene <ekeneezeifeomasam27@gmail.com>
2022-12-28 08:24:52 +0100

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,10 @@
+import Home from './HomePage'
+import Products from './ProductsPage'
+import SingleProduct from './SingleProductPage'
+import About from './AboutPage'
+import Error from './ErrorPage'
+import Checkout from './CheckoutPage'
+import PrivateRoute from './PrivateRoute'
+import Cart from './CartPage'
+
+export {Home, Products, SingleProduct, About, Error, Checkout, PrivateRoute, Cart}
